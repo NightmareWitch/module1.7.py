7b4c970fef6772df06ecf3dbd6817dbe3c7be774
NightmareWitch <jokerhell13@gmail.com>
2024-08-27 20:46:00 +0300

    Add files via upload

---
+++ modul1.py
@@ -0,0 +1,17 @@
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+number1 = [5, 3, 3, 5, 4]
+number2 = [2, 2, 2, 3]
+number3 = [4, 5, 5, 2]
+number4 = [4, 4, 3]
+number5 = [5, 5, 5, 4, 5]
+average = sum(number1) / len(number1)
+average2 = sum(number2) / len(number2)
+average3 = sum(number3) / len(number3)
+average4 = sum(number4) / len(number4)
+average5 = sum(number5) / len(number5)
+
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+tuple = {'Aaron': average, 'Bilbo': average2, 'Johnny': average3, 'Khendrik': average4, 'Steve': average5}
+print(grades)
+print(students)
+print(tuple)
